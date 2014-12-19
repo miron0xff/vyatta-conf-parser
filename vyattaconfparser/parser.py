2d6855e39125fcc0842e735aaef5a05146c218c2
Alexander Mironov <mironov@altell.ru>
2014-12-19 17:57:18 +0300

    Python3 support

--- vyattaconfparser/parser.py
+++ vyattaconfparser/parser.py
@@ -1,11 +1,21 @@
 # coding:utf-8
 import re
+import sys
 from collections import defaultdict
 
-rxh = re.compile(ur'^([\w\-]+) \{$', re.UNICODE)
-rxl = re.compile(ur'^([\w\-]+) ([\w\-\"\./]+) \{$', re.UNICODE)
-rxv = re.compile(ur'^([\w\-]+) ([\w\-\"\.,/:@\*»« ]+)$', re.UNICODE)
-rxu = re.compile(ur'^([\w\-]+)$', re.UNICODE)
+if sys.version < '3':
+    def u(x):
+        return x.decode('utf-8')
+else:
+    unicode = str
+
+    def u(x):
+        return x
+
+rxh = re.compile(r'^([\w\-]+) \{$', re.UNICODE)
+rxl = re.compile(r'^([\w\-]+) ([\w\-\"\./]+) \{$', re.UNICODE)
+rxv = re.compile(r'^([\w\-]+) ([\w\-\"\.,/:@\*»« ]+)$', re.UNICODE)
+rxu = re.compile(r'^([\w\-]+)$', re.UNICODE)
 
 
 class ParserException(Exception):
@@ -19,15 +29,15 @@ def tree():
 def update_tree(config, path, val):
     t = config
     for n, i in enumerate(path):
-        t = t[i.keys()[0]]
+        t = t[list(i.keys())[0]]
     if isinstance(t, dict):
         if isinstance(val, dict):
-            if val.keys()[0] not in t.keys():
+            if list(val.keys())[0] not in list(t.keys()):
                 t.update(val)
-            elif isinstance(t[val.keys()[0]], unicode):
-                t.update({val.keys()[0]: [t[val.keys()[0]], val.values()[0]]})
-            elif isinstance(t[val.keys()[0]], list):
-                t.update({val.keys()[0]: t[val.keys()[0]] + val.values()})
+            elif isinstance(t[list(val.keys())[0]], unicode):
+                t.update({list(val.keys())[0]: [t[list(val.keys())[0]], list(val.values())[0]]})
+            elif isinstance(t[list(val.keys())[0]], list):
+                t.update({list(val.keys())[0]: t[list(val.keys())[0]] + list(val.values())})
         else:
             t.update({val: {}})
     elif isinstance(t, list):
@@ -70,21 +80,21 @@ def parse_node(config, raw, headers=None, i=0):
         update_tree(config, headers, {kv: kv})
 
     elif line == '}' and headers:
-        hq = [h.values()[0] for h in headers]
+        hq = [list(h.values())[0] for h in headers]
         headers.pop()
         if len(hq) > 1 and hq[-2:] == ['l', 'd']:
             headers.pop()
 
     else:
-        raise ParserException(u'Parse error: "%s", Line: %d' % (line, i))
+        raise ParserException('Parse error: "%s", Line: %d' % (line, i))
 
     parse_node(config, raw, headers, i + 1)
 
 
 def parse_conf(string):
     if string:
-        string = string.decode('utf-8').split('\n')
+        string = u(string).split('\n')
         c = tree()
         parse_node(c, string)
         return c
-    raise ParserException(u'Empty config passed')
+    raise ParserException('Empty config passed')
