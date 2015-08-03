32e8a195243f755c0d23bf952454ae868eaef853
Alexander Mironov <mironov@altell.ru>
2015-08-03 13:20:23 +0300

    Update package version & add AUTHORS file

--- vyattaconfparser/__init__.py
+++ vyattaconfparser/__init__.py
@@ -1,8 +1,8 @@
 from .parser import parse_conf
 
 __title__ = 'Vyatta Config Parser'
-__version__ = '0.3'
+__version__ = '0.4.1'
 __author__ = 'Alexander Mironov'
 __license__ = 'MIT'
-__copyright__ = 'Copyright 2014 Alexander Mironov'
+__copyright__ = 'Copyright 2015 Alexander Mironov'
 __all__ = ['parse_conf']
