8486140ef7f775a9438b08bbabea328e002e3cdc
Alexander Mironov <mironov@altell.ru>
2015-11-11 12:23:35 +0300

    Update version for PyPI

--- vyattaconfparser/__init__.py
+++ vyattaconfparser/__init__.py
@@ -1,7 +1,7 @@
 from .parser import parse_conf
 
 __title__ = 'Vyatta Config Parser'
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 __author__ = 'Alexander Mironov'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2015 Alexander Mironov'
